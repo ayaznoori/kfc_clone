545fdb4104e0aff6f4aa6e80a794d1e84f9b5482
ayaznoori <ayaz.noori15@gmail.com>
2022-06-14 10:39:17 +0530

    aaded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './page/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-       
+       <Navbar/>
+       <Home/>
+       <Footer/>
     </div>
   );
 }
