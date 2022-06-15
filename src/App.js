64c271dd80f3c1ce6cd21a28bc2227cfb1691aeb
Devashishsaurabh <onlyvishwasindia@gmail.com>
2022-06-15 10:05:22 +0530

    Home Pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './page/Home';
 import Footer from './components/Footer';
+import { useState } from 'react';
 
 function App() {
+const [loading,setLoading]=useState(true)
+const spinner=document.getElementById("spinner")
+if(spinner){
+  setTimeout(()=>{
+    spinner.style.display="none";
+    setLoading(false)
+},2500)
+}
   return (
-    <div className="App">
-       <Navbar/>
-       <Home/>
-       <Footer/>
-    </div>
+    !loading && (
+      <div className='App'>
+        <Navbar />
+      <Home/>
+      <Footer />
+   </div>
+    )
   );
 }
 
